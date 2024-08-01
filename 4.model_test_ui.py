502549adde6bab84a7c82ece092494a483945449
marcolanfranchi <marcolanfranchi02@gmail.com>
2024-08-01 16:40:35 -0700

    Still working on README

--- 4.model_test_ui.py
+++ 4.model_test_ui.py
@@ -4,9 +4,12 @@ import numpy as np
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
-import pandas as pd 
+import pandas as pd
 import torch
 from sklearn.metrics.pairwise import cosine_similarity
+from streamlit_extras.let_it_rain import rain
+import streamlit_scrollable_textbox as stx
+import altair as alt
 
 load_dotenv()
 
@@ -15,63 +18,126 @@ openai_key = os.getenv('OPENAI_KEY')
 client = OpenAI(api_key=openai_key)
 submissions = pd.read_pickle('output/openai_embedded_large_all.pkl')
 
-# Load trained model (logarithmic regression)
+# Load trained model (logistic regression)
 with open('logregress_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-def get_embedding(text, model="text-embedding-3-large"): # Code snippet from https://platform.openai.com/docs/guides/embeddings/use-cases
-   """
-   Get the embedding of a text using the OpenAI API text-embedding-3-large model
-   """
-   text = text.replace("\n", " ")
-   embedding = torch.tensor(client.embeddings.create(input = [text], model=model).data[0].embedding)
-   embedding = np.array(embedding).reshape(1, -1)
-   return embedding
+# Load t-SNE data
+df_tsne = pd.read_pickle('output/tsne.pkl')
 
-# Define a function to make predictions
+
+@st.cache_data
+def get_embedding(text, model="text-embedding-3-large"):
+    text = text.replace("\n", " ")
+    embedding = torch.tensor(client.embeddings.create(input=[text], model=model).data[0].embedding)
+    embedding = np.array(embedding).reshape(1, -1)
+    return embedding
+
+
+@st.cache_data
 def predict(embedding):
-    """
-    Given some input text, convert the text to a vector and use the model to make a prediction
-    """
-    prediction = model.predict(embedding)[0]
+    prediction = model.predict(embedding.reshape(1, -1))[0]
     return 'Asshole' if prediction == 0 else 'Not the Asshole'
 
-def search_reviews(df, embedding, n=1):
-    """
-    find the top n most similar embeddings in the DataFrame.
-    """
-    # Ensure the input embedding is a 2D array (things break otherwise)
-    embedding = embedding.reshape(1, -1)
 
-    # Calculate cosine similarity for each embedding in the DataFrame (turning everything into 2D arrays)
+@st.cache_data
+def get_similar_posts(df, embedding, n=3):
     df['similarities'] = df['embedding'].apply(lambda x: cosine_similarity(np.array(x).reshape(1, -1), embedding).flatten()[0])
-    
-    # Sort the DataFrame by similarity and return the top n results
     res = df.sort_values('similarities', ascending=False).head(n)
     return res
-# initialize the streamlit app
+
+@st.cache_data
+def plot_tsne_with_annotations(similar_posts):
+    colors = {'Asshole': 'red', 'Not the A-hole': 'blue'}
+    color_scale = alt.Scale(domain=list(colors.keys()), range=list(colors.values()))
+
+    base_chart = alt.Chart(df_tsne).mark_circle(size=60, opacity=0.5).encode(
+        x='tsne1',
+        y='tsne2',
+        color=alt.Color('label', scale=color_scale),
+        tooltip=['label', 'selftext']
+    )
+
+    annotations = []
+    idx = 1
+    for index, row in similar_posts.iterrows():
+        embedding_data = df_tsne[df_tsne['selftext'] == row['selftext']]
+        annotations.append(alt.Chart(pd.DataFrame({
+            'title': '⬆ '+f'Similar Story {idx}',
+            'label': row['link_flair_text'],
+            'selftext': row['selftext'],
+            'tsne1': embedding_data['tsne1'],
+            'tsne2': embedding_data['tsne2']
+        })).mark_text(
+            align='left',
+            baseline='middle',
+            dx=7,
+            dy=-7,
+            fontWeight='bold',
+            fontSize=12,
+            color='white'
+        ).encode(
+            text='title',
+            tooltip=['label', 'selftext'],
+            x='tsne1',
+            y='tsne2'
+        ))
+        idx += 1
+
+    chart = base_chart + alt.layer(*annotations)
+
+    st.altair_chart(chart, use_container_width=True)
+
+
 st.set_page_config(page_title='Am I the Asshole?', page_icon='🤔', layout='centered', initial_sidebar_state='auto')
-st.title('Am I the Asshole?')
+st.title('Am I ?')
 
-story = st.text_area("Enter your story (max 250 characters):", "")
+def emoji_animation(model_result):
+    if model_result == 'Asshole':
+        emoji = "😡"
+    elif model_result == 'Not the Asshole':
+        emoji = "😊"
+    rain(
+        emoji=emoji,
+        font_size=54,
+        falling_speed=3,
+        animation_length="1s",
+    )
 
-if st.button('Ask'):
+
+story = st.text_area("Enter your story (max 500 characters):", "")
+
+if st.button('ask'):
     if story:
-        if len(story) <= 250:
+        if len(story) <= 500:
             embedding = get_embedding(story)
             result = predict(embedding)
-            st.write("Story: ", story)
-            # st.dataframe(pd.DataFrame(embedding))
-            st.write(f'You are: {result}')
-            res = search_reviews(submissions, embedding, n=1).iloc[0]
-            story = res['selftext']
-            classfication = res['link_flair_text']
-            st.write(f'This story is most similar to yours')
-            st.write(f'This story was classified as {classfication} by redditors')
-            st.write(f'{story}')
+            with st.container():
+                st.markdown(f'- **story:** {story}')
+                st.markdown(f'- **the model says you are:** {result}')
+                emoji_animation(result)
+                res = get_similar_posts(submissions, embedding, n=3)
+                st.markdown("these stories are most similar to yours:")
+                story_num = 1
+                for index, row in res.iterrows():
+                    with st.container():
+                        st.write('---')
+                        st.markdown(f'**{story_num})**')
+                        story_title = row['title']
+                        story = row['selftext']
+                        reddit_classification = row['link_flair_text']
+                        st.markdown(f'*{story_title}*')
+                        st.markdown(f'(this story was classified as **{reddit_classification}** by redditors)')
+                        stx.scrollableTextbox(story, height=250, fontFamily='system-ui')
+                        story_num += 1
 
+                # plotting t-SNE chart with annotations for similar stories
+                st.write("---")
+                st.write("This is where the most similar stories are located in the t-SNE plot:")
+                plot_tsne_with_annotations(res)
 
         else:
-            st.write("Please limit your story to 250 characters.")
+            st.write("Please limit your story to 500 characters.")
     else:
         st.write("Please enter a story to get a prediction.")
+
---
+++ README.md
@@ -0,0 +1,82 @@
+# README
+
+
+# AITA Predictor
+This project is a part of SFU's [CMPT 353 Summer 2024](https://www.sfu.ca/outlines.html?2024/summer/cmpt/353/d100), Computational Data Science. 
+
+<!-- #### -- Project Status: [Active, On-Hold, Completed] -->
+
+## Project Intro/Objective
+In this project, we aimed to develop a machine learning model capable of categorizing posts from [r/AmiTheAsshole](https://www.reddit.com/r/AmItheAsshole/). We collected data from 2022-2023 and narrowed it down to the two post flair categories that were most common: those being “YTA”(You’re The Asshole) or “NTA” (Not The Asshole). Our goal is to reliably predict the consensus of the community based on the content of each submission by categorizing it into one of those 2 categories. 
+
+### Methods Used
+* Inferential Statistics
+* Text Embeddings
+* Machine Learning
+* Data Visualization
+* Predictive Modeling
+
+### Technologies
+* Python
+* scikit-learn
+* Hadoop
+* Torch
+* Spark
+* Pandas
+* pickle
+* Jupyter
+* Streamlit
+
+## Before Starting
+- Our dataset of r/AmItheAsshole posts was collected directly from the Hadoop cluster provided as part of the course material in CMPT 353. We began our exploration by using data from 2023, but later expanded and included posts from 2022. This process is in ([`0. get_reddit_data.py`](0.get_reddit_data.py)). Since this process has a long run-time and did not need to be ran again, we saved the data to a directory of [zipped json files](). 
+    - If you wish to run the next couple of steps, the second of which will require an OpenAI api key for feature generation, you can download this data [here]() and place it in the ([**FILL THIS IN**]()) directory. Otherwise, you do not need this data.
+- After 
+- For the feature generation part of the project, [`2.Convert_OpenAI_Embedding.ipynb`](2.Convert_OpenAI_Embedding), we generated vector embeddings for the text of each post using OpenAI's [`text-embedding-3-large`](https://platform.openai.com/docs/guides/embeddings) embedding model. This file also has a long run-time, and requires an OpenAI Api key.
+    - If you wish to run `2.Convert_OpenAI_Embedding.ipynb`, download the data mentioned above and create a `.env` file in the root directory and add your OpenAI Api key to the file:
+    ```OPENAI_KEY='your_openai_api_key'```
+    - Otherwise, the dataset including the vector embedding for each row created from this file is located at [xxx](), so you can proceed without executing this file.
+
+## Getting Started
+
+1. Clone this repo and open a terminal in the root directory.
+
+    ```
+    git clone https://github.com/Localbrownboy/AITA_Predictor.git
+
+    cd AITA_predictor
+    ```
+
+2. If you wish to run [`1. unload_data.py`](1.unload_data.py):
+    - SSH 
+    
+3. Data processing/transformation scripts are being kept [here](Repo folder containing data processing scripts/notebooks)
+4. etc...
+
+*If your project is well underway and setup is fairly complicated (ie. requires installation of many packages) create another "setup.md" file and link to it here*  
+
+5. Follow setup [instructions](Link to file)
+
+## Featured Notebooks/Analysis/Deliverables
+* [Notebook/Markdown/Slide Deck Title](link)
+* [Notebook/Markdown/Slide DeckTitle](link)
+* [Blog Post](link)
+
+
+## Contributing DSWG Members
+
+**Team Leads (Contacts) : [Full Name](https://github.com/[github handle])(@slackHandle)**
+
+#### Other Members:
+
+|Name     |  Slack Handle   | 
+|---------|-----------------|
+|[Full Name](https://github.com/[github handle])| @johnDoe        |
+|[Full Name](https://github.com/[github handle]) |     @janeDoe    |
+
+## Contact
+* If you haven't joined the SF Brigade Slack, [you can do that here](http://c4sf.me/slack).  
+* Our slack channel is `#datasci-projectname`
+* Feel free to contact team leads with any questions or if you are interested in contributing!
+
+
+
